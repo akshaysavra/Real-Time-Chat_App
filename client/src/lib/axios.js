e1d831985e627258b8d88f30eecb44a091353b49
akshaysavra <savaraakshay2366@gmail.com>
2025-11-25 06:51:23 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api/v1" : "/api/v1",
+    baseURL: "https://real-time-chat-app-backend-n0dd.onrender.com/api/v1",
     withCredentials: true,
 
 });
+
